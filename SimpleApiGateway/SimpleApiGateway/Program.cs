a36a869727484d419dd490ab3b36abdc6d65e50a
Cormac Smith <47928489+CormacS@users.noreply.github.com>
2025-02-17 22:53:58 +0000

    JWT Auth

---
+++ SimpleApiGateway/SimpleApiGateway/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SimpleApiGateway;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<TokenService>();
+
+var secretKey = builder.Configuration["JwtSettings:SecretKey"];
+
+// Add authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = TokenService.GetSigningKey(secretKey),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
